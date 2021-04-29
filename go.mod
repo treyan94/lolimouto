f017e7207a757ba4ee81c8699fffab758c7844a4
Ruben Iritsyan <treyan94@gmail.com>
2021-04-29 12:54:36 +0400

    `/s` and `y/n`

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
-module "lolimouto"
+module lolimouto
+
+go 1.16
+
+require gopkg.in/tucnak/telebot.v2 v2.3.5
--- lolimouto.go
+++ lolimouto.go
@@ -1,5 +1,62 @@
 package main
 
-func main() {
+import (
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
 
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+var apiKey = func() (key string) {
+	key = os.Getenv("LOLIMOUTO_BOT_KEY")
+
+	if args := os.Args[1:]; len(args) != 0 {
+		key = args[0]
+	}
+
+	if key == "" {
+		log.Fatal("provide telegram bot api key as first argument")
+	}
+
+	return key
+}()
+
+func main() {
+	b, err := tb.NewBot(tb.Settings{
+		Token:  apiKey,
+		Poller: &tb.LongPoller{Timeout: 1 * time.Second},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle(tb.OnText, func(m *tb.Message) {
+		if strings.HasPrefix(m.Text, "s/") && m.ReplyTo != nil {
+			split := strings.Split(m.Text, "/")
+
+			if len(split) < 3 {
+				return
+			}
+			replyTo := m.ReplyTo
+
+			b.Reply(replyTo, strings.Replace(replyTo.Text, split[1], split[2], -1))
+
+			return
+		}
+
+		textLower := strings.ToLower(m.Text)
+		if strings.HasSuffix(textLower, " y/n") || strings.HasPrefix(textLower, "y/n") {
+			responses := []string{"Yes", "No"}
+			i := rand.Intn(len(responses))
+
+			b.Send(m.Chat, responses[i])
+		}
+	})
+
+	b.Start()
 }
