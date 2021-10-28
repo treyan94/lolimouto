2e24a2a7c1ca3eda0df60fc13dec63274b392bec
Ruben Iritsyan <treyan94@gmail.com>
2021-10-28 12:51:15 +0400

    `Weather.Emoji` method

--- openweathermap/weather.go
+++ openweathermap/weather.go
@@ -8,3 +8,46 @@ type Weather struct {
 	Description string `json:"description"`
 	Icon        string `json:"icon"`
 }
+
+func (w Weather) Emoji() string {
+	switch w.Icon {
+	case "01d":
+		return "☀️"
+	case "01n":
+		return "🌃"
+	case "02d":
+		return "🌤️"
+	case "02n":
+		return "🌁"
+	case "03d":
+		fallthrough
+	case "03n":
+		fallthrough
+	case "04d":
+		fallthrough
+	case "04n":
+		return "☁️"
+	case "09d":
+		fallthrough
+	case "09n":
+		fallthrough
+	case "10n":
+		return "🌧️"
+	case "10d":
+		return "🌦️"
+	case "11d":
+		fallthrough
+	case "11n":
+		return "⛈️"
+	case "13d":
+		fallthrough
+	case "13n":
+		return "🌨️"
+	case "50d":
+		fallthrough
+	case "50n":
+		return "🌫️"
+	default:
+		return "❓"
+	}
+}
